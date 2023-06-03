8ff4e2647080b43b3e59a47793ddd092e8ac846b
James Attrill <attrill20@gmail.com>
2023-06-03 22:43:43 +0100

    dropdown added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -129,6 +129,15 @@ export default function App() {
       <div className="sub-heading">
         <h2>Welcome to the Fixture Difficulty Planner for the 2022/23 Fixtures. You can use this site to filter teams by upcoming GWs to help plan transfers for players with easier fixtures and maximise your returns. Best of luck!</h2>
       </div>
+      <div class="filter-container">
+        <div className="gameweek-dropdown">
+            <select id="gameweek-dropdown">
+              <option value="GW1">1 Gameweek</option>
+              <option value="GW2">2 Gameweeks</option>
+              <option value="GW3">3 Gameweeks</option>
+            </select>
+        </div>
+      </div>
       <div className = "cards">
         <CardList teams={teams} gw1={gw1} gw2={gw2} gw3={gw3}/>
       </div>

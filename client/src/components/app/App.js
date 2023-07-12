bb68947eb9aaa29d8726568120b3edf403b721b2
James Attrill <attrill20@gmail.com>
2023-07-12 16:20:04 +0100

    added a package.json to fpl-fixtures

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import CardList from "../cardlist/cardlist";
 import Dropdown from "../dropdown/dropdown";
@@ -7,6 +7,7 @@ import { teams, gameweeks } from "../dummy arrays/dummy";
 // rendering the react elements
 export default function App() {
   const [numberOfGameweeks, setNumberOfGameweeks] = useState(1);
+  const [data, setData] = useState(null);
 
   const handleGameweekChange = (event) => {
     setNumberOfGameweeks(event.target.value);
@@ -14,13 +15,16 @@ export default function App() {
 
   let filteredGameweeks = gameweeks.slice(0, numberOfGameweeks);
 
-  async function fetchFPL() {
+  useEffect(() => {
+    async function fetchFPL() {
     const response = await fetch("http://localhost:3005")
     const data = await response.json();
+    setData(data);
     console.log(data);
     console.log(data.teams[0].name)
   }
   fetchFPL()
+}, []);
 
   return (
     <div className="app">
@@ -29,10 +33,14 @@ export default function App() {
       </header>
       <div className="sub-heading">
         <h2>
-          Welcome to the Fixture Difficulty Planner for the 2022/23 Fixtures.
+          Welcome to the Fixture Difficulty Planner for the 2023/24 Fixtures.
           You can use this site to filter teams by upcoming GWs to help plan
           transfers for players with easier fixtures and maximise your returns.
           Best of luck!
+        </h2>
+        <h2>
+          <p>Test: {data && data.teams[0].name}</p>
+          <p>Test: {data && data.teams[19].name}</p>
         </h2>
       </div>
       <div className="filter-container">

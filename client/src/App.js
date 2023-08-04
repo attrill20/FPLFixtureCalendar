401214358296ab81d275e0ec40421439a8e84e59
James Attrill <attrill20@gmail.com>
2023-08-04 19:50:28 +0100

    further refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -2,45 +2,15 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import CardList from "./components/cardlist/cardlist";
 import PlayerSearcher from "./components/playerSearcher/playerSearcher";
-import Dropdown from "./components/dropdown/dropdown";
 import { teams } from "./components/dummyArrays/dummy";
 // import { Routes, Route } from "react-router-dom";
 
 export default function App() {
-  const [numberOfGameweeks, setNumberOfGameweeks] = useState(5);
+
   const [data, setData] = useState(null);
   const [mainData, setMainData] = useState(null);
   const [fixturesData, setFixturesData] = useState(null);
-  const [sortOrder, setSortOrder] = useState("desc"); 
-
-  const handleGameweekChange = (event) => {
-    setNumberOfGameweeks(event.target.value);
-  };
-
-  function getFixturesForTeam(teamId, numberOfFixtures) {
-    if (!fixturesData) {
-      return { fixtures: [], totalDifficulty: 0, reversedTotalDifficulty: 0 };
-    }
-    
-    const teamFixtures = fixturesData.filter(
-      fixture => fixture.team_h === teamId || fixture.team_a === teamId
-    );
-
-    // Calculate the total difficulty and return the requested number of fixtures
-    const nextFixtures = teamFixtures.slice(0, numberOfFixtures).map(fixture => {
-      const opponent = fixture.team_h === teamId ? teams[fixture.team_a].name : teams[fixture.team_h].name;
-      const home = fixture.team_h === teamId;
-      const difficulty = home ? fixture.team_h_difficulty : fixture.team_a_difficulty;
-      return { opponent, home, difficulty };
-    });
   
-    // Calculate the total difficulty score
-    const totalDifficulty = nextFixtures.reduce((acc, fixture) => acc + fixture.difficulty, 0);
-    const reversedTotalDifficulty = (numberOfFixtures * 5) - totalDifficulty
-  
-    // Return the requested number of fixtures and the total difficulty
-    return { fixtures: nextFixtures, totalDifficulty, reversedTotalDifficulty };
-  }
 
   // Fetch the FPL API data
   useEffect(() => {
@@ -55,17 +25,9 @@ export default function App() {
   fetchFPL()
 }, []);
 
-const handleTableReorder = () => {
-  setSortOrder((prevSortOrder) => (prevSortOrder === "asc" ? "desc" : "asc"));
-};
 
-const modifiedTeams = [];
-for (const team of teams) {
-  const { id } = team;
-  const { reversedTotalDifficulty } = getFixturesForTeam(id, numberOfGameweeks);
-  const modifiedTeam = { ...team, FDR: reversedTotalDifficulty };
-  modifiedTeams.push(modifiedTeam);
-};
+
+
 
 
   return (
@@ -82,24 +44,10 @@ for (const team of teams) {
         </h2>
       </div>
 
-   
-
-    <div className="filter-container">
-       <div className="gameweek-dropdown">
-          <Dropdown handleGameweekChange={handleGameweekChange} />
-        </div>
-    </div>
-
-        <button onClick={handleTableReorder}>
-          Reorder Table by FDR {sortOrder === "asc" ? "↑" : "↓"}
-        </button> 
-
       <div className="cards">
         <CardList
-          teams={modifiedTeams} 
+          teams={teams}
           fixturesData={fixturesData}
-          numberOfGameweeks={numberOfGameweeks}
-          sortOrder={sortOrder} 
         />
       </div>
 
@@ -109,7 +57,7 @@ for (const team of teams) {
         mainData={mainData}
       />
     </div>
-    
+
     </div>
   );
 }
--- client/src/components/cardlist/cardlist.js
+++ client/src/components/cardlist/cardlist.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Row from "../card/card";
+import Dropdown from "../dropdown/dropdown";
+import "./cardlist.css";
 
-export default function CardList({ teams, fixturesData, numberOfGameweeks, sortOrder }) {
-  // 1. Create a copy of the teams array to avoid mutating the original data
-  const sortedTeams = [...teams];
+export default function CardList({ teams, fixturesData }) {
+  const [numberOfGameweeks, setNumberOfGameweeks] = useState(5);
+  const [sortOrder, setSortOrder] = useState("desc");
 
-  // 2. Sort the teams array based on the "FDR" values and the sortOrder
+  function getFixturesForTeam(teamId, numberOfFixtures) {
+    if (!fixturesData) {
+      return { fixtures: [], totalDifficulty: 0, reversedTotalDifficulty: 0 };
+    }
+
+    const teamFixtures = fixturesData.filter(
+      (fixture) => fixture.team_h === teamId || fixture.team_a === teamId
+    );
+
+    // Calculate the total difficulty and return the requested number of fixtures
+    const nextFixtures = teamFixtures
+      .slice(0, numberOfFixtures)
+      .map((fixture) => {
+        const opponent =
+          fixture.team_h === teamId
+            ? teams[fixture.team_a].name
+            : teams[fixture.team_h].name;
+        const home = fixture.team_h === teamId;
+        const difficulty = home
+          ? fixture.team_h_difficulty
+          : fixture.team_a_difficulty;
+        return { opponent, home, difficulty };
+      });
+
+    // Calculate the total difficulty score
+    const totalDifficulty = nextFixtures.reduce(
+      (acc, fixture) => acc + fixture.difficulty,
+      0
+    );
+    const reversedTotalDifficulty = numberOfFixtures * 5 - totalDifficulty;
+
+    // Return the requested number of fixtures and the total difficulty
+    return { fixtures: nextFixtures, totalDifficulty, reversedTotalDifficulty };
+  }
+
+  const handleGameweekChange = (event) => {
+    setNumberOfGameweeks(event.target.value);
+  };
+
+  const handleTableReorder = () => {
+    setSortOrder((prevSortOrder) => (prevSortOrder === "asc" ? "desc" : "asc"));
+  };
+
+  const modifiedTeams = teams.map((team) => {
+    const { id } = team;
+    const { reversedTotalDifficulty } = getFixturesForTeam(
+      id,
+      numberOfGameweeks
+    );
+    return { ...team, FDR: reversedTotalDifficulty };
+  });
+
+  const sortedTeams = [...modifiedTeams];
   sortedTeams.sort((teamA, teamB) => {
     const fdrA = teamA.FDR;
     const fdrB = teamB.FDR;
@@ -17,16 +71,26 @@ export default function CardList({ teams, fixturesData, numberOfGameweeks, sortO
   });
 
   return (
-    <div className="table">
-      {sortedTeams.slice(1).map((team, index) => (
-        <Row
-          teams={teams}
-          fixturesData={fixturesData}
-          teamIndex={team.id}
-          numberOfFixtures={numberOfGameweeks}
-          key={index}
-        />
-      ))}
+    <div>
+      <div className="gameweek-dropdown">
+        <Dropdown handleGameweekChange={handleGameweekChange} />
+      </div>
+
+      <button onClick={handleTableReorder}>
+        Reorder Table by FDR {sortOrder === "asc" ? "↑" : "↓"}
+      </button>
+
+      <div className="table">
+        {sortedTeams.slice(1).map((team, index) => (
+          <Row
+            teams={modifiedTeams}
+            fixturesData={fixturesData}
+            teamIndex={team.id}
+            numberOfFixtures={numberOfGameweeks}
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   );
 }

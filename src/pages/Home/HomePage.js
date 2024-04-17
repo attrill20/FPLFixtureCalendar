1a013b81bc7d65cda24c560cdaa32c3304ab5efd
James Attrill <attrill20@gmail.com>
2024-04-17 21:44:48 +0100

    added AI image to homepage

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import "./HomePage.css"
 import { Link } from 'react-router-dom';
+import oracle_fpl_3 from '../../components/images/oracle_fpl_3.jpeg';
 
 const HomePage= () => {
   return (
     <div>
     <header className="app-header">
-        <h1>Hello, welcome to my home page!</h1>
+        <h1>Welcome to OracleFPL</h1>
       </header>
       <div className="sub-heading">
+        <img src={oracle_fpl_3} className="oracle-fpl-image" alt="OracleFPL" />
         <h3>
-         This is the site!
+         This is the homepage of the OracleFPL tools - we hope they hope you find them useful and improve your FPL rank! Use the Navbar above to select the tool you need.
         </h3> 
       </div>
     </div>

5ea86a03b6641909ca3ca7246df7dea2a8b5aa78
James Attrill <attrill20@gmail.com>
2024-07-17 22:20:44 +0100

    updated teams for 2024/25 season

--- src/components/dummyArrays/dummy.js
+++ src/components/dummyArrays/dummy.js
@@ -9,8 +9,9 @@ import CHEbadge from '../badges/CHEbadge.png'
 import CRYbadge from '../badges/CRYbadge.png'
 import EVEbadge from '../badges/EVEbadge.png'
 import FULbadge from '../badges/FULbadge.png'
-// import LEIbadge from '../badges/LEIbadge.png'
-// import LEEbadge from '../badges/LEEbadge.png'
+import IPSbadge from '../badges/IPSbadge.png'
+import LEIbadge from '../badges/LEIbadge.png'
+import LEEbadge from '../badges/LEEbadge.png'
 import LIVbadge from '../badges/LIVbadge.png'
 import LUTbadge from '../badges/LUTbadge.png'
 import MCIbadge from '../badges/MCIbadge.png'
@@ -18,7 +19,7 @@ import MUNbadge from '../badges/MUNbadge.png'
 import NEWbadge from '../badges/NEWbadge.png'
 import NFObadge from '../badges/NFObadge.png'
 import SHUbadge from '../badges/SHUbadge.png'
-// import SOUbadge from '../badges/SOUbadge.png'
+import SOUbadge from '../badges/SOUbadge.png'
 import TOTbadge from '../badges/TOTbadge.png'
 import WHUbadge from '../badges/WHUbadge.png'
 import WOLbadge from '../badges/WOLbadge.png'
@@ -32,21 +33,22 @@ export let teams = [
   { id: 3, name: "Bournemouth", initial: "BOU", badge: BOUbadge, h_diff: 4, a_diff: 4 },
   { id: 4, name: "Brentford", initial: "BRE", badge: BREbadge, h_diff: 3, a_diff: 6 },
   { id: 5, name: "Brighton", initial: "BHA", badge: BHAbadge, h_diff: 5, a_diff: 7 },
-  { id: 6, name: "Burnley", initial: "BUR", badge: BURbadge, h_diff: 2, a_diff: 1 },
-  { id: 7, name: "Chelsea", initial: "CHE", badge: CHEbadge, h_diff: 5, a_diff: 4 },
-  { id: 8, name: "Crystal Palace", initial: "CRY", badge: CRYbadge, h_diff: 5, a_diff: 2 },
-  { id: 9, name: "Everton", initial: "EVE", badge: EVEbadge, h_diff: 6, a_diff: 3 },
-  { id: 10, name: "Fulham", initial: "FUL", badge: FULbadge, h_diff: 3, a_diff: 6 },
-  // { name: "Leicester City", initial: "LEI", badge: LEIbadge },
+  // { id: 6, name: "Burnley", initial: "BUR", badge: BURbadge, h_diff: 2, a_diff: 1 },
+  { id: 6, name: "Chelsea", initial: "CHE", badge: CHEbadge, h_diff: 5, a_diff: 4 },
+  { id: 7, name: "Crystal Palace", initial: "CRY", badge: CRYbadge, h_diff: 5, a_diff: 2 },
+  { id: 8, name: "Everton", initial: "EVE", badge: EVEbadge, h_diff: 6, a_diff: 3 },
+  { id: 9, name: "Fulham", initial: "FUL", badge: FULbadge, h_diff: 3, a_diff: 6 },
+  { id: 10, name: "Ipswich", initial: "IPS", badge: IPSbadge, h_diff: 3, a_diff: 6 },
+  { id: 11, name: "Leicester", initial: "LEI", badge: LEIbadge, h_diff: 3, a_diff: 6 },
   // { name: "Leeds United", initial: "LEE", badge: LEEbadge },
-  { id: 11, name: "Liverpool", initial: "LIV", badge: LIVbadge, h_diff: 7, a_diff: 10 },
-  { id: 12, name: "Luton", initial: "LUT", badge: LUTbadge, h_diff: 2, a_diff: 2 },
+  { id: 12, name: "Liverpool", initial: "LIV", badge: LIVbadge, h_diff: 7, a_diff: 10 },
+  // { id: 13, name: "Luton", initial: "LUT", badge: LUTbadge, h_diff: 2, a_diff: 2 },
   { id: 13, name: "Man City", initial: "MCI", badge: MCIbadge, h_diff: 7, a_diff: 9 },
   { id: 14, name: "Man Utd", initial: "MUN", badge: MUNbadge, h_diff: 6, a_diff: 6 },
   { id: 15, name: "Newcastle", initial: "NEW", badge: NEWbadge, h_diff: 3, a_diff: 9 },
   { id: 16, name: "Notts Forest", initial: "NFO", badge: NFObadge, h_diff: 2, a_diff: 4 },
-  // { name: "Southampton", initial: "SOU", badge: SOUbadge },
-  { id: 17, name: "Sheffield Utd", initial: "SHU", badge: SHUbadge, h_diff: 1, a_diff: 4 },
+  { id: 17, name: "Southampton", initial: "SOU", badge: SOUbadge, h_diff: 3, a_diff: 6 },
+  // { id: 17, name: "Sheffield Utd", initial: "SHU", badge: SHUbadge, h_diff: 1, a_diff: 4 },
   { id: 18, name: "Spurs", initial: "TOT", badge: TOTbadge, h_diff: 7, a_diff: 6 },
   { id: 19, name: "West Ham", initial: "WHU", badge: WHUbadge, h_diff: 6, a_diff: 5 },
   { id: 20, name: "Wolves", initial: "WOL", badge: WOLbadge, h_diff: 3, a_diff: 5 },
--- src/pages/FixtureCalendarPage/FixtureCalendarPage.js
+++ src/pages/FixtureCalendarPage/FixtureCalendarPage.js
@@ -8,7 +8,7 @@ const FixtureCalendarPage= ({ teams, fixturesData, activeGameweek }) => {
     <div>
       <div className="sub-heading">
         <p>
-         The table can be sorted using the buttons below using either our custom 1-10 <a href="https://docs.google.com/spreadsheets/d/1wYS6wyRkHiP-9zlJAkd_V9vjnxXslpNinz-CLg6BILs/edit#gid=0" target="_blank" rel="noopener noreferrer">Oracle FDR</a> (Fixture Difficulty Rating) or the Official 1-5 FPL FDR to generate a total difficulty score for each team. Set the active gameweek forwards to plan future transfers!
+         The table can be sorted using the buttons below using either our custom 1-10 <a href="https://docs.google.com/spreadsheets/d/1wYS6wyRkHiP-9zlJAkd_V9vjnxXslpNinz-CLg6BILs/edit#gid=0" target="_blank" rel="noopener noreferrer">Oracle FDR</a> (Fixture Difficulty Rating) or the Official 1-5 FPL FDR to generate a total difficulty score for each team. Set the active gameweek forwards to plan future transfers! 
         </p> 
       </div>
 

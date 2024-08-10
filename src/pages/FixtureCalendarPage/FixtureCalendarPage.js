30266bccf8290f8a642a763da9d6fbeb783cfb79
James Attrill <attrill20@gmail.com>
2024-08-10 18:04:59 +0100

    navbar tweaks

--- src/pages/FixtureCalendarPage/FixtureCalendarPage.js
+++ src/pages/FixtureCalendarPage/FixtureCalendarPage.js
@@ -7,7 +7,7 @@ const FixtureCalendarPage= ({ teams, fixturesData, activeGameweek }) => {
     <div>
       <div className="fixture-sub-heading">
         <p>
-         The table can be sorted using the buttons below using either our custom 1-10 <a href="https://docs.google.com/spreadsheets/d/1wYS6wyRkHiP-9zlJAkd_V9vjnxXslpNinz-CLg6BILs/edit#gid=0" target="_blank" rel="noopener noreferrer">Oracle FDR</a> (Fixture Difficulty Rating) or the Official 1-5 FPL FDR to generate a total difficulty score for each team. Set the active gameweek to plan future transfers! 
+         The table can be sorted using the buttons below using either our custom 1-10 <a href="https://docs.google.com/spreadsheets/d/1wYS6wyRkHiP-9zlJAkd_V9vjnxXslpNinz-CLg6BILs/edit#gid=0" target="_blank" rel="noopener noreferrer">Oracle FDR</a> (Fixture Difficulty Rating) or the official 1-5 FPL FDR to display a total difficulty score for each team. Change the active gameweek to plan future transfers! 
         </p> 
       </div>
 

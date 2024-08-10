e9713cce79d9bf06b7dd8ac28fa84b22c386bf3c
James Attrill <attrill20@gmail.com>
2024-08-10 19:52:12 +0100

    added brand name to navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -11,14 +11,20 @@ function Navbar() {
 
   return (
     <nav className="navbar">
+      <NavLink to="/home" className="navbar-left">
+        {/* <img src="path/to/logo.png" alt="Logo" className="navbar-logo" /> */}
+        <span className="navbar-brand anton-regular">OracleFPL</span>
+      </NavLink>
+      <div className="navbar-menu-container">
+        <ul className={`navbar-menu ${isOpen ? 'open' : ''}`}>
+          <li><NavLink exact to="/home" activeClassName="active" onClick={toggleMenu}>Home</NavLink></li>
+          <li><NavLink exact to="/calendar" activeClassName="active" onClick={toggleMenu}>Fixture Difficulty Calendar</NavLink></li>
+          <li><NavLink exact to="/comparison" activeClassName="active" onClick={toggleMenu}>Player Comparison</NavLink></li>
+        </ul>
+      </div>
       <button className="navbar-toggler" onClick={toggleMenu}>
         &#9776; {/* Hamburger icon */}
       </button>
-      <ul className={`navbar-menu ${isOpen ? 'open' : ''}`}>
-        <li><NavLink exact to="/home" activeClassName="active" onClick={toggleMenu}>Home</NavLink></li>
-        <li><NavLink exact to="/calendar" activeClassName="active" onClick={toggleMenu}>Fixture Difficulty Calendar</NavLink></li>
-        <li><NavLink exact to="/comparison" activeClassName="active" onClick={toggleMenu}>Player Comparison</NavLink></li>
-      </ul>
     </nav>
   );
 }

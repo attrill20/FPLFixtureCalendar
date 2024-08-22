0b2660b880f04aef89bcc7215da381193f544aba
James Attrill <attrill20@gmail.com>
2024-08-22 23:53:42 +0100

    margin css changes

--- src/pages/PlayerComparisonPage/PlayerComparisonPage.js
+++ src/pages/PlayerComparisonPage/PlayerComparisonPage.js
@@ -1,81 +1,3 @@
-// import React from 'react';
-// import PlayerComparison from '../../components/playerComparison/playerComparison';
-// import "./PlayerComparisonPage.css";
-
-// const PlayerComparisonPage= ({ data, mainData }) => {
-//   return (
-//     <div>
-//       <div className="player-pics">
-// 		{data && (
-// 			<div>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${480455}.png`}
-// 					alt="Branthwaite"
-// 				/>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${516895}.png`}
-// 					alt="Mainoo"
-// 				/>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${219847}.png`}
-// 					alt="Havertz"
-// 				/>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${178301}.png`}
-// 					alt="Watkins"
-// 				/>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${219168}.png`}
-// 					alt="Isakr"
-// 				/>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${178301}.png`}
-// 					alt="Watkins"
-// 				/>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${219168}.png`}
-// 					alt="Isakr"
-// 				/>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${219168}.png`}
-// 					alt="Isakr"
-// 				/>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${178301}.png`}
-// 					alt="Watkins"
-// 				/>
-// 				<img
-// 					className="player-pic"
-// 					src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${219168}.png`}
-// 					alt="Isak"
-// 				/>
-// 			</div>
-// 		)}
-// 	</div>
-// 	<div className="search-sub-heading">
-// 		<p> Not sure who to pick? Compare player stats using the side by side Player Comparison tool below! </p>
-// 		<p> Type in a player's surname to see a range of FPL stats displayed for them (check spelling / official FPL name if it doesn't display): </p>
-// 	</div>
-// 	<div className="player-comparison-tool">
-// 		<PlayerComparison data={data} mainData={mainData} />
-// 	</div>
-      
-//     </div>
-//   );
-// };
-
-// export default PlayerComparisonPage;
-
-
 import React from 'react';
 import PlayerComparison from '../../components/playerComparison/playerComparison';
 import "./PlayerComparisonPage.css";
@@ -89,7 +11,7 @@ const PlayerComparisonPage = ({ data, mainData }) => {
     <div>
       <div className="player-pics">
         {data && (
-          <div>
+          <div className="pics-wrapper">
           	<img
 				className="player-pic"
 				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[0].code}.png`}

57645b7d47e6d8e36a6831b3d885c60e0b8882e6
James Attrill <attrill20@gmail.com>
2024-08-22 23:57:27 +0100

    refactor of how player data loads

--- src/pages/PlayerComparisonPage/PlayerComparisonPage.js
+++ src/pages/PlayerComparisonPage/PlayerComparisonPage.js
@@ -3,65 +3,25 @@ import PlayerComparison from '../../components/playerComparison/playerComparison
 import "./PlayerComparisonPage.css";
 
 const PlayerComparisonPage = ({ data, mainData }) => {
+  // Ensure mainData and mainData.elements are defined and are arrays
+  const elements = mainData && Array.isArray(mainData.elements) ? mainData.elements : [];
 
   // Sort the players by event points
-  const sortedPlayers = mainData.elements.sort((a, b) => b.event_points - a.event_points);
+  const sortedPlayers = elements.sort((a, b) => b.event_points - a.event_points);
 
   return (
     <div>
       <div className="player-pics">
-        {data && (
+        {elements.length > 0 && (
           <div className="pics-wrapper">
-          	<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[0].code}.png`}
-				alt="player-1"
-			/>
-			<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[1].code}.png`}
-				alt="player-2"
-			/>
-			<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[2].code}.png`}
-				alt="player-3"
-			/>
-			<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[3].code}.png`}
-				alt="player-4"
-			/>
-			<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[4].code}.png`}
-				alt="player-5"
-			/>
-			<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[5].code}.png`}
-				alt="player-6"
-			/>
-			<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[6].code}.png`}
-				alt="player-7"
-			/>
-			<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[7].code}.png`}
-				alt="player-8"
-			/>
-			<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[8].code}.png`}
-				alt="player-9"
-			/>
-			<img
-				className="player-pic"
-				src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${sortedPlayers[9].code}.png`}
-				alt="player-10"
-			/>
+            {sortedPlayers.slice(0, 10).map((player, index) => (
+              <img
+                key={player.code}
+                className="player-pic"
+                src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`}
+                alt={`player-${index + 1}`}
+              />
+            ))}
           </div>
         )}
       </div>
@@ -71,9 +31,9 @@ const PlayerComparisonPage = ({ data, mainData }) => {
         <p> Type in a player's surname to see a range of FPL stats displayed for them (check spelling / official FPL name if it doesn't display): </p>
       </div>
 
-	  <div className="player-comparison-tool">
-		<PlayerComparison data={data} mainData={mainData} />
-	  </div>
+      <div className="player-comparison-tool">
+        <PlayerComparison data={data} mainData={mainData} />
+      </div>
     </div>
   );
 };

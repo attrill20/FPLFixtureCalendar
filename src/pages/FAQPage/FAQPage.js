5bbf2ce6c479d61bb3c7e3a6cde64bd02f3cde55
James Attrill <attrill20@gmail.com>
2024-09-06 11:26:38 +0100

    added FAQ copy

--- src/pages/FAQPage/FAQPage.js
+++ src/pages/FAQPage/FAQPage.js
@@ -40,45 +40,56 @@ const FAQPage = () => {
 
   const faqs2 = [
     {
-      question: "What is this page about? HI",
+      question: "What is the Player Comparison Tool?",
       answer: [
-        "This page provides answers to some of the most frequently asked questions.",
-        "Feel free to explore and learn more about our offerings."
+        "This allows the comparison between two FPL players of their current season stats, to help decide which would be the better pick. Enter their surname (or preferred player name from their shirt) and then select them from the dropdown of matching players.",
+        "By default it displays their player stats, selection stats, points stats, attacking stats and bonus stats. If you wish to add defending stats or remove the attacking stats then these can be customised via the checkboxes."
       ]
     },
     {
-      question: "How do I use this FAQ page? HI",
+      question: "What are the xG, xA and xGC stats?",
       answer: [
-        "Simply click on any question to expand and see the answer."
+        "These are very useful stats that are being used more frequently in modern football to determine the underlying performance of players, and stand for expected goals, expected assists and expected goals conceded. These are calculated based on the likeliness of that specific shot or pass resulting in a goal or assist, for example a shot from the 6-yard box in the middle of the goal would result in an xG of 0.9+ whereas a shot from long distance at a wide angle would be less than 0.1. xGC is the total of all these shots that teams have accumulated throughout the season",
+        "This can be used to identify players that are over or underperforming their underlying stats, and decisions can be based on the assumption that these figures are often likely to regress to the mean over the course of a whole season. Whether you decide to pick players that have good underlying numbers and hope they eventually come good, or prefer to back players in form to continue overperforming their xG - well that choice is up to you!"
       ]
     },
     {
-      question: "Can I add more questions? HI",
+      question: "What are the per 90 stats?",
       answer: [
-        "Yes, you can easily add more questions by modifying the FAQ data in the code."
+        "These take the total stats and divide these per 90 minutes played for each player. This is useful to identify more proficient strikers, for example a striker that has scored 5 goals in 1000 minutes will have a higher goals per 90 figure than one who has scored 10 goals in 3000 minutes so could be a better pick than purely judging on total goals scored.",
+        "These can also be applied to defenders by looking at the goals conceded per 90 to identify defences that are likely to get clean sheets. Total clean sheets are also displayed, with a percentage per start as players likely need to start in order to get to 60 minutes and claim the clean sheet points."
       ]
     }
   ];
 
   const faqs3 = [
     {
-      question: "What is this page about? NOW",
+      question: "Who is The Oracle?",
       answer: [
-        "This page provides answers to some of the most frequently asked questions.",
-        "We are constantly updating this section with new content to help our users."
+        "The Oracle is the FPL alter ego of me - James, an FPL enthusiast and software developer from Brighton.",
+        "This site is a personal passion project to help bring to fruition some useful FPL tools that I had envisioned and practice my coding skills. The original objective of the site was to create a more useful fixture difficulty calendar using a custom rating system to help identify players with better fixtures and get an edge in FPL!"
+      ]
+    },
+    { 
+      question: "What are your historic performances on FPL?",
+      answer: [
+        "Not great! I've been an avid FPL fan for almost 20 years, my account says I've played every season since 07/08 but I'm sure I've been playing even longer than that on the old site. My best ever finish is a not especially impressive 36k in 08/09.",
+        "I've only really been taking it more seriously in the last few years having gotten interested in understanding how the player price changes worked and how to build team value by planning transfers properly. My current benchmark is to finish in the top 1% of managers, which I achieved in 21/22 (67k) and 22/23 (75k), but missed out on in a disappointing 23/24 (178k)."
       ]
     },
     {
-      question: "How do I use this FAQ page? NOW",
-      answer: [
-        "Simply click on any question to expand and see the answer."
-      ]
+        question: "What is Beat The Robot?",
+        answer: [
+          "Beat The Robot is an ongoing competition for the 23/24 season that pits Erling 'The Robot' Haaland against a different opposition player each week to see who scores the most FPL points. This experiment arose from the fact that he was so heavily captained last season, I wanted to see if his total score could be beaten by selecting a different player with a better fixture as generated from the Fixture Difficulty Calendar.",
+          'The ongoing total will be tracked on the Beat The Robot tab on the <a href="https://docs.google.com/spreadsheets/d/1wYS6wyRkHiP-9zlJAkd_V9vjnxXslpNinz-CLg6BILs/edit#gid=0" target="_blank" rel="noopener noreferrer">Oracle FDR Google Sheet</a> and weekly updates posted on <a href="https://x.com/OracleFPL1" target="_blank" rel="noopener noreferrer">The Oracle X Account</a>.',
+        ]
     },
     {
-      question: "Can I add more questions? NOW",
-      answer: [
-        "Yes, you can easily add more questions by modifying the FAQ data in the code."
-      ]
+        question: "Where else can I stay in touch with The Oracle?",
+        answer: [
+          'Follow updates on <a href="https://x.com/OracleFPL1" target="_blank" rel="noopener noreferrer">The Oracle X Account</a> for FPL team reveals, GW points, FDR updates and Beat The Robot.',
+          'Alternatively, if you have any queries, please direct them to <a href="mailto:oraclefplcontact@gmail.com">oraclefplcontact@gmail.com</a>.'
+        ]
     }
   ];
 
@@ -104,7 +115,7 @@ const FAQPage = () => {
         ))}
       </div>
 
-      {/* <div className="faq-section-container">
+      <div className="faq-section-container">
         <h3 className="faq-section-title">2. Player Comparison Tool</h3>
         {faqs2.map((faq, index) => (
           <div key={`section2-${index}`} className="faq-content">
@@ -126,11 +137,15 @@ const FAQPage = () => {
               {faq.question}
             </h2>
             {expandedSections["section3"]?.[index] && faq.answer.map((paragraph, i) => (
-              <p key={`answer3-${index}-${i}`} className="faq-answer">{paragraph}</p>
+                <p
+                key={`answer1-${index}-${i}`}
+                className="faq-answer"
+                dangerouslySetInnerHTML={{ __html: paragraph }}
+              />
             ))}
           </div>
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };

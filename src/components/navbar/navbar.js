6ad1da8dfb6ea0917370ffeedaeca997a53350c5
James Attrill <attrill20@gmail.com>
2024-09-06 11:34:44 +0100

    added navbar routing and css changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -19,6 +19,7 @@ function Navbar() {
           <li><NavLink exact to="/home" activeClassName="active" onClick={toggleMenu}>Home</NavLink></li>
           <li><NavLink exact to="/calendar" activeClassName="active" onClick={toggleMenu}>Fixture Difficulty Calendar</NavLink></li>
           <li><NavLink exact to="/comparison" activeClassName="active" onClick={toggleMenu}>Player Comparison</NavLink></li>
+          <li><NavLink exact to="/faq" activeClassName="active" onClick={toggleMenu}>FAQs</NavLink></li>
         </ul>
       </div>
       <button className="navbar-toggler" onClick={toggleMenu}>

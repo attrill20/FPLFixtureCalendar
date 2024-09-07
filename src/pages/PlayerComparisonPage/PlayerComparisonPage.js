8e5638911768e2665d4a1c0fdef321120f9ed6f5
James Attrill <attrill20@gmail.com>
2024-09-07 21:06:03 +0100

    more top 10s

--- src/pages/PlayerComparisonPage/PlayerComparisonPage.js
+++ src/pages/PlayerComparisonPage/PlayerComparisonPage.js
@@ -10,7 +10,11 @@ const PlayerComparisonPage = ({ data, mainData }) => {
   const sortedPlayersForm = [...elements].sort((a, b) => b.form - a.form);
   const sortedPlayersOwnership = [...elements].sort((a, b) => b.selected_by_percent - a.selected_by_percent);
   const sortedPlayersXGI = [...elements].sort((a, b) => b.expected_goal_involvements - a.expected_goal_involvements);
-
+  const top10Goalkeepers = [...elements].filter(player => player.element_type === 1).sort((a, b) => b.total_points - a.total_points) 
+  const top10Defenders = [...elements].filter(player => player.element_type === 2).sort((a, b) => b.total_points - a.total_points) 
+  const top10Midfielders = [...elements].filter(player => player.element_type === 3).sort((a, b) => b.total_points - a.total_points) 
+  const top10Forwards = [...elements].filter(player => player.element_type === 4).sort((a, b) => b.total_points - a.total_points) 
+   
   return (
     <div>
       {/* <div className="player-pics">
@@ -125,7 +129,84 @@ const PlayerComparisonPage = ({ data, mainData }) => {
                   alt={`player-${index + 1}`}
                 />
                 <p className="player-stat-name">{player.web_name}</p>
-                <p className="player-stat">{player.expected_goal_involvements} ({player.goals_scored + player.assists})</p>
+                <p className="player-stat">{player.expected_goal_involvements}</p>
+                <p className="player-stat">({player.goals_scored + player.assists})</p> 
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="player-pics player-pics-lists">
+        <p className="top-10-title">Top 10 Goalkeepers</p> 
+        {elements.length > 0 && (
+          <div className="pics-wrapper">
+            {top10Goalkeepers.slice(0, 10).map((player, index) => (
+              <div key={player.code} className="player-pic-container">
+                <img
+                  className="player-pic-top-10"
+                  src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`}
+                  alt={`player-${index + 1}`}
+                />
+                <p className="player-stat-name">{player.web_name}</p>
+                <p className="player-stat">{player.total_points}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="player-pics player-pics-lists">
+        <p className="top-10-title">Top 10 Defenders</p> 
+        {elements.length > 0 && (
+          <div className="pics-wrapper">
+            {top10Defenders.slice(0, 10).map((player, index) => (
+              <div key={player.code} className="player-pic-container">
+                <img
+                  className="player-pic-top-10"
+                  src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`}
+                  alt={`player-${index + 1}`}
+                />
+                <p className="player-stat-name">{player.web_name}</p>
+                <p className="player-stat">{player.total_points}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="player-pics player-pics-lists">
+        <p className="top-10-title">Top 10 Midfielders</p> 
+        {elements.length > 0 && (
+          <div className="pics-wrapper">
+            {top10Midfielders.slice(0, 10).map((player, index) => (
+              <div key={player.code} className="player-pic-container">
+                <img
+                  className="player-pic-top-10"
+                  src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`}
+                  alt={`player-${index + 1}`}
+                />
+                <p className="player-stat-name">{player.web_name}</p>
+                <p className="player-stat">{player.total_points}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="player-pics player-pics-lists">
+        <p className="top-10-title">Top 10 Forwards</p> 
+        {elements.length > 0 && (
+          <div className="pics-wrapper">
+            {top10Forwards.slice(0, 10).map((player, index) => (
+              <div key={player.code} className="player-pic-container">
+                <img
+                  className="player-pic-top-10"
+                  src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`}
+                  alt={`player-${index + 1}`}
+                />
+                <p className="player-stat-name">{player.web_name}</p>
+                <p className="player-stat">{player.total_points}</p>
               </div>
             ))}
           </div>

2ebdbe9fcda4d03ea7136686e2ef4fc347f332b5
James Attrill <attrill20@gmail.com>
2024-11-23 20:26:32 +0000

    reordered over / under

--- src/pages/Top10Page/Top10Page.js
+++ src/pages/Top10Page/Top10Page.js
@@ -405,33 +405,6 @@ const Top10Page = ({ mainData, teams, fixturesData }) => {
       </div>
 
       <div className="player-pics player-pics-lists">
-        <p className="top-10-title">Top 10 xG Under Performance (Goals) </p> 
-        {elements.length > 0 && (
-          <div className="pics-wrapper">
-            {filterPlayers(sortedPlayersXGUnderPerformance).slice(0, 10).map((player, index) => (
-              <div key={player.code} className="player-pic-container">
-                <img
-                  className="player-pic-top-10"
-                  src={`https://resources.premierleague.com/premierleague/photos/players/250x250/p${player.code}.png`}
-                  alt={`player-${index + 1}`}
-                  onError={(e) => {
-                      if (player.element_type === 1) {
-                        e.target.src = `https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_${player.team_code}_1-110.png`;
-                      } else {
-                        e.target.src = `https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_${player.team_code}-110.png`;
-                      }
-                    }}
-                />
-                <p className="player-stat-name">{player.web_name}</p>
-                <p className="player-stat">{(player.goals_scored - player.expected_goals).toFixed(2)}</p>
-                <p className="player-stat">({player.goals_scored})</p> 
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-
-      <div className="player-pics player-pics-lists">
         <p className="top-10-title">Top 10 xG Over Performance (Goals) </p> 
         {elements.length > 0 && (
           <div className="pics-wrapper">
@@ -451,6 +424,33 @@ const Top10Page = ({ mainData, teams, fixturesData }) => {
                 />
                 <p className="player-stat-name">{player.web_name}</p>
                 <p className="player-stat">+{(player.goals_scored - player.expected_goals).toFixed(2)}</p>
+                <p className="player-stat">({player.goals_scored})</p> 
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="player-pics player-pics-lists">
+        <p className="top-10-title">Top 10 xG Under Performance (Goals) </p> 
+        {elements.length > 0 && (
+          <div className="pics-wrapper">
+            {filterPlayers(sortedPlayersXGUnderPerformance).slice(0, 10).map((player, index) => (
+              <div key={player.code} className="player-pic-container">
+                <img
+                  className="player-pic-top-10"
+                  src={`https://resources.premierleague.com/premierleague/photos/players/250x250/p${player.code}.png`}
+                  alt={`player-${index + 1}`}
+                  onError={(e) => {
+                      if (player.element_type === 1) {
+                        e.target.src = `https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_${player.team_code}_1-110.png`;
+                      } else {
+                        e.target.src = `https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_${player.team_code}-110.png`;
+                      }
+                    }}
+                />
+                <p className="player-stat-name">{player.web_name}</p>
+                <p className="player-stat">{(player.goals_scored - player.expected_goals).toFixed(2)}</p>
                 <p className="player-stat">({player.goals_scored})</p> 
               </div>
             ))}

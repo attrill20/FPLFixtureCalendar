f1329c6fca512084480e14e8df58876bd06e0173
James Attrill <attrill20@gmail.com>
2026-02-07 16:33:12 +0000

    adds new link to fdr

--- src/pages/FixtureCalendarPage/FixtureCalendarPage.js
+++ src/pages/FixtureCalendarPage/FixtureCalendarPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CardList from '../../components/cardlist/cardlist';
 import "./FixtureCalendarPage.css"
 
@@ -7,8 +8,8 @@ const FixtureCalendarPage = ({ teams, fixturesData, activeGameweek }) => {
     <div>
       <div className="fixture-sub-heading">
         <p>
-         The table can be sorted using the buttons below using either our custom 1-10 <a href="https://docs.google.com/spreadsheets/d/1wYS6wyRkHiP-9zlJAkd_V9vjnxXslpNinz-CLg6BILs/edit#gid=0" target="_blank" rel="noopener noreferrer">Oracle FDR</a> (Fixture Difficulty Rating) or the official 1-5 FPL FDR to display a total difficulty score for each team. Change the active GW to plan future transfers! 
-        </p> 
+         The table can be sorted using the buttons below using either our custom 1-10 <Link to="/fdr-comparison">Oracle FDR</Link> (Fixture Difficulty Rating) or the official 1-5 FPL FDR to display a total difficulty score for each team. Change the active GW to plan future transfers!
+        </p>
       </div>
 
     <CardList teams={teams} fixturesData={fixturesData} activeGameweek={activeGameweek} />
